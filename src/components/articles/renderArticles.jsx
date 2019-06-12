b06640fc603b2553d2439517b6f193217d96a5c8
Tom Robinson <tom6rr@aol.com>
2019-06-12 09:17:25 +0100

    typo

--- src/components/articles/renderArticles.jsx
+++ src/components/articles/renderArticles.jsx
@@ -9,12 +9,12 @@ export default class RenderArticles extends Component {
     const { articles, maxPage, loggedInUser, changePage, filterItems, title, query } = this.props;
     return (
       <div>
+        <PageButtons maxPages={maxPage} changePage={changePage} />
+        <h3>{title}</h3>
         {!articles && <Loader type="Puff"
           color="#00BFFF"
           height="100"
           width="100" />}
-        <PageButtons maxPages={maxPage} changePage={changePage} />
-        <h3>{title}</h3>
         <SortBy filterItems={filterItems} query={query} columnsToSort={['votes', 'comment count', 'created at']} />
         {articles && <ArticleList articles={articles} loggedInUser={loggedInUser} />}
       </div>
